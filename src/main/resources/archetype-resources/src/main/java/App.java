850c726d51e96f2646a4d3a6602ed37fad2404d5
Paul N. Baker <paul.nelson.baker@gmail.com>
2019-06-26 16:26:28 -0600

    Adjusting whitespace

--- src/main/resources/archetype-resources/src/main/java/App.java
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -2,11 +2,10 @@ package $io.paulbaker.archetypes;
 
 /**
  * Hello world!
- *
  */
 public class App {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
