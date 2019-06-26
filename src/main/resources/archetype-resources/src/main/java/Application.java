e4e6ac48af7b1d030cfeaf2b8e195355c0aa2915
Paul N. Baker <paul.nelson.baker@gmail.com>
2019-06-26 16:30:10 -0600

    Renaming files to show be more clear

--- src/main/resources/archetype-resources/src/main/java/Application.java
+++ src/main/resources/archetype-resources/src/main/java/Application.java
@@ -1,9 +1,9 @@
-package $io.paulbaker.archetypes;
+package ${package};
 
 /**
  * Hello world!
  */
-public class App {
+public class Application {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
---
+++ src/main/resources/archetype-resources/src/test/java/ApplicationTest.java
@@ -0,0 +1,16 @@
+package ${package};
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple Application.
+ */
+public class ApplicationTest {
+
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
