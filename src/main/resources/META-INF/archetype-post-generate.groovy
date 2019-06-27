aa2957d821c14a74fcf0ccf52d316eaa9ca95d83
Paul N. Baker <paul.nelson.baker@gmail.com>
2019-06-27 13:54:00 -0600

    Can initialize and include a .gitignore

--- src/main/resources/META-INF/archetype-post-generate.groovy
+++ src/main/resources/META-INF/archetype-post-generate.groovy
@@ -1,4 +1,12 @@
-"git init .".execute()
-"git add .".execute()
-"git commit -am 'Initial commit\n" +
+import java.nio.file.Path
+import java.nio.file.Paths
+
+// We need to change directory to where the project actually was generated.
+Path projectPath = Paths.get(request.getOutputDirectory(), request.getArtifactId())
+// Now we need to initialize and create all the git version control stuff
+"git -C $projectPath init .".execute()
+def url = "https://www.gitignore.io/api/vim,osx,linux,emacs,nanoc,eclipse,windows,intellij,jetbrains,sublimetext,intellij+iml,intellij+all,jetbrains+iml,jetbrains+all,microsoftoffice,exercism,visualstudio,visualstudiocode,openframeworks+visualstudio,serverless,java,java-web,code-java"
+//"curl '$url' -o '$projectPath/.gitignore'".execute()
+"git -C $projectPath add *".execute()
+"git -C $projectPath commit -am 'Initial commit\n" +
         "Generated via `mvn archetype:generate -DarchetypeGroupId=io.paulbaker.archetypes -DarchetypeArtifactId=project-quickstart`"
