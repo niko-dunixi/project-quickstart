be88fed2d00d4b8425a06cf88f1ad21e40307b09
Paul N. Baker <paul.nelson.baker@gmail.com>
2019-06-27 12:53:31 -0600

    Post generation script

---
+++ src/main/resources/META-INF/archetype-post-generate.groovy
@@ -0,0 +1,4 @@
+"git init .".execute()
+"git add .".execute()
+"git commit -am 'Initial commit\n" +
+        "Generated via `mvn archetype:generate -DarchetypeGroupId=io.paulbaker.archetypes -DarchetypeArtifactId=project-quickstart`"
