82a44afc04597a7970db5bc6623b159b0f6366d0
Paul N. Baker <paul.nelson.baker@gmail.com>
2019-06-27 16:25:23 -0600

    PostScript can intialize a clean git repository

--- src/main/resources/META-INF/archetype-post-generate.groovy
+++ src/main/resources/META-INF/archetype-post-generate.groovy
@@ -1,12 +1,37 @@
-import java.nio.file.Path
 import java.nio.file.Paths
 
-// We need to change directory to where the project actually was generated.
-Path projectPath = Paths.get(request.getOutputDirectory(), request.getArtifactId())
+dir = Paths.get(request.getOutputDirectory(), request.getArtifactId()).toFile()
+
+def run(String... cmd) {
+//    println ' -> ' + cmd
+    def process = cmd.execute(null, dir)
+//    def outputBuilder = new StringBuilder()
+//    process.waitForProcessOutput(outputBuilder, System.err)
+//    String output = outputBuilder.toString()
+//            .replaceAll("\n{2,}", "\n") // Replace two or more new lines with just one...
+//            .replaceAll("\n", "\n\t") // Replace each new line with a new line and a tab...
+//            .trim() // remove any excess
+//    if (!output.isEmpty()) {
+//        println '\t' + output
+//    }
+    process.waitForProcessOutput((Appendable) System.out, System.err)
+    if (process.exitValue() != 0) {
+        throw new Exception("Command '$cmd' exited with code: ${process.exitValue()}")
+    }
+}
+
+
+// Make project portable.
+run("mvn", "-N", "io.takari:maven:wrapper")
+//run("./mvnw sortpom:sort")
+//run("./mvnw license:update-file-header@config")
+//run("./mvnw license:update-file-header@sources")
+//run("./mvnw fmt:format")
+
 // Now we need to initialize and create all the git version control stuff
-"git -C $projectPath init .".execute()
-def url = "https://www.gitignore.io/api/vim,osx,linux,emacs,nanoc,eclipse,windows,intellij,jetbrains,sublimetext,intellij+iml,intellij+all,jetbrains+iml,jetbrains+all,microsoftoffice,exercism,visualstudio,visualstudiocode,openframeworks+visualstudio,serverless,java,java-web,code-java"
-//"curl '$url' -o '$projectPath/.gitignore'".execute()
-"git -C $projectPath add *".execute()
-"git -C $projectPath commit -am 'Initial commit\n" +
-        "Generated via `mvn archetype:generate -DarchetypeGroupId=io.paulbaker.archetypes -DarchetypeArtifactId=project-quickstart`"
+run("git", "init", ".")
+run("git", "add", ".")
+String commitMessage = "Initial commit\n" +
+        "Generated via mvn archetype:generate -DarchetypeGroupId=io.paulbaker.archetypes -DarchetypeArtifactId=project-quickstart\n" +
+        "Report issues: https://github.com/paul-nelson-baker/project-quickstart"
+run("git", "commit", "-am", commitMessage)
